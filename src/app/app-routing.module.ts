2d919f604ea9eca277d0726082f13513487b37b0
Nathan Pickard <nathan.pickard87@gmail.com>
2018-06-22 20:05:03 -0700

    Additional project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HackerListComponent } from './hacker-list/hacker-list.component';
+import { StatsComponent } from './stats/stats.component';
+import { HackerDetailComponent } from './hacker-detail/hacker-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'hackers',
+    pathMatch: 'full'
+  },
+  {
+    path: 'hackers',
+    component: HackerListComponent
+  },
+  {
+    path: 'hackers/:id',
+    component: HackerDetailComponent
+  },
+  {
+    path: 'stats',
+    component: StatsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
